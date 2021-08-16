8b8c870b17861e94defb2326bd94b2a5e12584e4
PhoneWalletKeys <rsvetter91@gmail.com>
2021-08-16 13:18:54 -0700

    added sessions controller and more models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get "/notes" => "notes#index"
+  get "/notes/:id" => "notes#show"
   post "/users" => "users#create"
-  root 'notes#index'
-  resources :notes
+  post "/sessions" => "sessions#create"
+  post "/notes" => "notes#create"
+  patch "/notes/:id" => "notes#update"
+  delete "/notes/:id" => "notes#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
