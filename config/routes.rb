29804f33d019eef75b9c12003cedca84e8e048a8
PhoneWalletKeys <rsvetter91@gmail.com>
2021-08-16 13:48:11 -0700

    added fixes to my views/layouts pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get "/notes" => "notes#index"
   get "/notes/:id" => "notes#show"
   post "/users" => "users#create"
-  post "/sessions" => "sessions#create"
-  post "/notes" => "notes#create"
   patch "/notes/:id" => "notes#update"
   delete "/notes/:id" => "notes#destroy"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/sessions" => "sessions#create"
+
+  post "/notes" => "notes#create"
 end
